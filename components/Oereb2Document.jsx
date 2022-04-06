4c0a1f129932d44ec12eca117c2d744f86ff8049
Sandro Mani <manisandro@gmail.com>
2022-04-06 15:48:08 +0200

    Handle localized text in regulations/responsible office links

--- components/Oereb2Document.jsx
+++ components/Oereb2Document.jsx
@@ -148,7 +148,7 @@ class OerebDocument extends React.Component {
         for (const entry of entries) {
             for (const prov of this.ensureArray(entry.LegalProvisions)) {
                 regulations[this.localizedText(prov.TextAtWeb)] = {
-                    label: this.localizedText(prov.Title) + (prov.OfficialNumber ? ", " + prov.OfficialNumber : ""),
+                    label: this.localizedText(prov.Title) + (prov.OfficialNumber ? ", " + this.localizedText(prov.OfficialNumber) : ""),
                     link: this.localizedText(prov.TextAtWeb)
                 };
                 for (const ref of this.ensureArray(prov.Reference)) {
@@ -166,7 +166,7 @@ class OerebDocument extends React.Component {
                 }
                 respoffices[prov.ResponsibleOffice.OfficeAtWeb] = {
                     label: this.localizedText(prov.ResponsibleOffice.Name),
-                    link: prov.ResponsibleOffice.OfficeAtWeb
+                    link: this.localizedText(prov.ResponsibleOffice.OfficeAtWeb)
                 };
             }
         }
@@ -174,7 +174,7 @@ class OerebDocument extends React.Component {
             respoffices = entries.reduce((res, restr) => {
                 res[restr.ResponsibleOffice.OfficeAtWeb] = {
                     label: this.localizedText(restr.ResponsibleOffice.Name),
-                    link: restr.ResponsibleOffice.OfficeAtWeb
+                    link: this.localizedText(restr.ResponsibleOffice.OfficeAtWeb)
                 };
                 return res;
             }, {});
